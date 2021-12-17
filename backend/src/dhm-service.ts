ecc149b89648f0944046c096def746e699da47d5
elisbihl <elis.bihl.syne20lin@tucsweden.se>
2021-12-17 16:22:56 +0100

    filter

--- backend/src/dhm-service.ts
+++ backend/src/dhm-service.ts
@@ -1,7 +1,10 @@
-import { DatabaseURI, DBQuery, URI } from '@divine/uri';
+import { DatabaseURI, DBQuery, DBResult, toObject, URI } from '@divine/uri';
 import { WebArguments, WebResource, WebService } from '@divine/web-service'
 import { API } from '@onslip/onslip-360-node-api';
 import { Name } from 'ajv';
+import { profileEnd } from 'console';
+import { emitKeypressEvents } from 'readline';
+import { createContext } from 'vm';
 import { DHMConfig } from './schema';
 
 export class DHMService {
@@ -11,7 +14,7 @@ export class DHMService {
     constructor(private config: DHMConfig) {
         const { base, realm, id, key } = config.onslip360;
         this.api = new API(base, realm, id, key);
-        this.db  = new URI(config.database.uri) as DatabaseURI;
+        this.db = new URI(config.database.uri) as DatabaseURI;
     }
 
     async initialize(): Promise<this> {
@@ -33,14 +36,48 @@ export class DHMService {
     }
 
     private async rootResponse(who?: string) {
-        const clientInfo = await this.api.getClientInfo()
-        const dbVersion  = await this.db.query<DBVersion[]>`select version()`;
-        const addTable = await this.db.query<DBQuery[]>`CREATE TABLE IF NOT EXISTS accounts (id INT PRIMARY KEY, balance INT)`;
-        this.db.query<DBQuery[]>`INSERT INTO accounts (id, balance) VALUES (1, 1000), (2, 250)`;
-        return [ 'Tony', who ?? clientInfo.user?.name, dbVersion[0].version ];
+        const clientInfo = await this.api.getClientInfo();
+        // const dbVersion = await this.db.query<DBVersion[]>`select version()`;
+        // const dbGetList = await this.db.query<DBQuery[]>`select * from onslip.products `;
+        const getGroups = await this.api.listProductGroups();
+        // this.db.query<DBQuery[]>`CREATE TABLE onslip.productcategories (id INT PRIMARY KEY, name STRING)`;
+        
+        const getProducts = await this.api.listProducts();
+      
+        // this.db.query<DBQuery[]>`CREATE TABLE onslip.products (name STRING, price STRING, description STRING, productcategory_id INT REFERENCES onslip.productcategories(id))`
+        // getGroups.forEach(element => 
+        //     this.db.query<DBQuery[]>`INSERT INTO onslip.productcategories (id, name) VALUES (${element.id}, ${element.name})`
+        //     );
+        // getProducts.forEach(element => 
+        //     this.db.query<DBQuery[]>`INSERT INTO onslip.products (rowid, name, price, description, productcategory_id) VALUES (${element.id}, ${element.name}, ${element.price ?? null}, ${element.description ?? null}, ${element['product-group']})`
+        //     );
+        return this.GetProdByGroup();
+    }
+
+
+
+    private async GetProdByGroup() {
+        const catName = await this.db.query<DBCat[]>`select * from onslip.productcategories`;
+        // const list = await this.db.query<DBQuery[]>`select onslip.products.name, onslip.productcategory.name from onslip.productcategory join onslip.products on productcategory_id = onslip.productcategory.rowid `;
+        // const catId = await this.db.query<DBQuery[]>`select rowid from onslip.productcategories`;
+        const prod = await this.db.query<DBProduct[]>`select * from onslip.products`;
+        return catName.map(r => [r.name, prod.filter(x => x.productcategory_id == r.id).map(x => x.name)]);
     }
 }
 
 interface DBVersion {
     version: string;
+}
+
+interface DBCat 
+{
+    id: number;
+    name: string;
+}  
+
+interface DBProduct {
+    name: string;
+    price: string;
+    description: string;
+    productcategory_id: number;
 }
