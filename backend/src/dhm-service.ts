c8c8b098144b90e641a917b9c02ac15055e73dc3
Martin-R-Peterson <martin.peterson.1993@gmail.com>
2021-12-21 10:11:39 +0100

    eventstream

--- backend/src/dhm-service.ts
+++ backend/src/dhm-service.ts
@@ -1,8 +1,10 @@
-import { DatabaseURI, DBQuery, DBResult, HEADERS, URI } from '@divine/uri';
+import { DatabaseURI, DBQuery, DBResult, FIELDS, HEADERS, JSONParser, URI, ZlibEncoder } from '@divine/uri';
 //const { DatabaseURI, URI } = require('@divine/uri');
 
 import { WebArguments, WebResource, WebService } from '@divine/web-service'
-import { API } from '@onslip/onslip-360-node-api';
+import { AbortController, API } from '@onslip/onslip-360-node-api';
+import { Name } from 'ajv';
+import { setPriority } from 'os';
 import { DHMConfig } from './schema';
 
 export class DHMService {
@@ -33,8 +35,23 @@ export class DHMService {
                         return svc.SelectFoodName(args.number('?food', undefined));
 
                     }
-                    else {
+                    if(args.has("?who")) {
                         return svc.rootResponse(args.string('?who', undefined));
+                    }
+                    if (args.has("?group")) {
+                        return svc.GetProdByGroup();
+
+                    }
+                    if (args.has("?trigg")) {
+                        return svc.ProductUpdateTrigger;
+
+                    }
+                    // if (args.has("?event")) {
+                    //     return svc.Eventstreamer();
+
+                    // }
+                    else {
+                        return "ERROR NOT FOUND" ;
                     }
 
 
@@ -89,16 +106,69 @@ export class DHMService {
 
 
     private async SelectFoodName(id?: number) {
-        const dbGetProducts = await this.db.query<DBProducts[]>`SELECT * FROM onslip.products`;
-        const dbGetCategory = await this.db.query<DBCategories[]>`SELECT * FROM onslip.productcategory WHERE rowid=${id?? 0}`;
+        const dbGetProducts = await this.db.query<DBProduct[]>`SELECT * FROM onslip.products`;
+        const dbGetCategory = await this.db.query<DBCategories[]>`SELECT * FROM onslip.productcategories WHERE id=${id?? 0}`;
 
 
         const filterProductsByCatID = await dbGetProducts.filter(x => x.productcategory_id == id);
-        console.log(filterProductsByCatID.map(x => x.productcategory_id));
 
-        return [await dbGetCategory.map(x => x.name),await filterProductsByCatID.map(x => x.name), await filterProductsByCatID.map(x => x.price)];
+        return [await dbGetCategory.map(x => x.name),await filterProductsByCatID.map(x => [x.name, x.price]),  this.Eventstreamer()];
     }
 
+
+    private async SelectFoodNameGroup(id?: number) {
+        const dbGetCategory = await this.db.query<DBCategories[]>`SELECT * FROM onslip.productcategory`;
+        const dbGetProducts = await this.db.query<DBProduct[]>` SELECT * FROM onslip.products,onslip.productcategory WHERE productcategory_id = onslip.productcategory.rowid `;
+
+
+        const getCatListName=  await dbGetCategory.map(x => x.name);
+        const getProdNamePrice = dbGetProducts.map(x=> [x.name,x.price]);
+        
+        //console.log(dbGetProducts.map(x => [x.price, x.name, x.productcategory_id, x.productcategoryname, x.]));
+        console.log(dbGetProducts);
+
+
+        
+        Symbol
+        
+
+        const filterProductsByCatID = await dbGetProducts.filter(x => x.productcategory_id == id);
+
+        return [await {getCatListName,}];
+    }
+
+    private async GetProdByGroup() {
+        const catName = await this.db.query<DBCat[]>`select * from onslip.productcategories`;
+        const prod = await this.db.query<DBProduct[]>`select * from onslip.products`;
+        return catName.map(r => [r.name, prod.filter(x => x.productcategory_id == r.id).map(x => x.name)]);
+    }
+    
+private async ProductUpdateTrigger() {
+    const trigger = await this.api.getTrigger(1,5);
+    console.log(trigger.query);
+}
+
+private async Eventstreamer(){
+    while(true){
+    try{
+    const stream = await this.api.addEventStream({ state: 'pending', queries: [ { resource: 'records', query: 'usage.type=product-update' } ] });
+    const cancelsignal = new AbortController();
+    setTimeout(() => cancelsignal.abort, 60_000);
+    
+
+    for await (const event of this.api.signal(cancelsignal.signal).openEventStream(stream.id)){
+        console.log(event);
+    }
+
+    console.log("done");
+
+
+    }catch(err){
+
+        console.log(`No error`)
+    }
+}
+}
 
 
 
@@ -106,7 +176,11 @@ export class DHMService {
 
 }
 
-
+interface DBCat 
+{
+    id: number;
+    name: string;
+}  
 
 interface DBVersion {
     version: string;
@@ -117,8 +191,16 @@ interface DBCategories {
     name: string;
 }
 
-interface DBProducts {
+interface DBProduct {
     name: string;
     price: string;
     productcategory_id: number;
+    productcategoryname: string;
+}
+
+
+
+interface QueryTrigger {
+    'resource': string;
+    'query': string;
 }
