eff7e7ca4a18957cc8345cdff5268142cdf7792c
elisbihl <elis.bihl.syne20lin@tucsweden.se>
2021-12-22 14:23:43 +0100

    clean up

--- backend/src/dhm-service.ts
+++ backend/src/dhm-service.ts
@@ -21,7 +21,7 @@ export class DHMService {
     this.db = new URI(config.database.uri) as DatabaseURI;
     this.listener = new Listener(config);
   }
-  
+
   async initialize(): Promise<this> {
     return this;
   }
@@ -42,8 +42,6 @@ export class DHMService {
 
   private async rootResponse(who?: string) {
     const clientInfo = await this.api.getClientInfo();
-    // this.listener.ListenerDelete();
-    // this.ListenerUpdate();
     this.listener.Listener();
     return this.GetProdByGroup();
   }
@@ -51,153 +49,16 @@ export class DHMService {
   private async GetProdByGroup() {
     const catName = await this.db.query<
       DBCat[]
-    >`select * from onslip.productcategories`;
+      >`select * from onslip.productcategories`;
     const prod = await this.db.query<
       DBProduct[]
-    >`select * from onslip.products`;
+      >`select * from onslip.products`;
     return catName.map((r) => [
       r.name,
       prod
         .filter((x) => x.productcategory_id == r.id)
         .map((x) => [x.name, x.price, x.description]),
     ]);
-  }
-
-//   private async ListenerUpdate() {
-//     while (true) {
-//       try {
-//         for (const pg of await this.api.listProductGroups("", "name")) {
-//           console.log(`  ${pg.name}, created on ${new Date(pg.created)}`);
-//         }
-
-//         for (const product of await this.api.listProducts(
-//           `price>20`,
-//           "name",
-//           0,
-//           -1,
-//           true,
-//           ""
-//         )) {
-//           console.log(
-//             `  ${product.name}: ${product.price} kr ${
-//               product.deleted
-//                 ? `(deleted on ${new Date(product.deleted as string)})`
-//                 : ""
-//             }`
-//           );
-//         }
-//         const stream = await this.api.addEventStream({
-//           state: "pending",
-//           queries: [
-//             { resource: "records", query: "usage.type=product-update" },
-//             { resource: "records", query: "usage.type=productgroup-update" },
-//           ],
-//         });
-//         const cancel = new AbortController();
-//         setTimeout(() => cancel.abort, 60_000);
-
-//         console.log("Product and product group updates:");
-//         for await (const event of this.api
-//           .signal(cancel.signal)
-//           .openEventStream(stream.id)) {
-//           const listProduct = this.api.listProducts();
-//           const listGroups = this.api.listProductGroups();
-//           (await listProduct).forEach(
-//             (element) =>
-//               this.db.query<DBQuery[]>`update onslip.products SET (name, price, description, productcategory_id) = (${element.name}, ${element.price ?? null}, ${element.description ?? null}, ${element["product-group"]}) where rowid = ${element.id}`
-//           );
-//           (await listGroups).forEach(
-//             (element) =>
-//               this.db.query<DBQuery[]>`update onslip.products SET (name) = (${element.name}) where rowid = ${element.id}`
-//           );
-//           console.log(event);
-//         }
-//         console.log("All done");
-//       } catch (error) {
-//         console.log(`OK`);
-//         await new Promise((resolve) => setTimeout(resolve));
-//       }
-//     }
-//   }
-
-//   private async ListenerCreate()
-//   {
-//       while(true)
-//       {
-//           try{
-//             const stream = await this.api.addEventStream({
-//                 state: "pending",
-//                 queries: [
-//                   { resource: "records", query: "usage.type=product-create" },
-//                   { resource: "records", query: "usage.type=productgroup-create" },
-//                 ],
-//               });
-//               const cancel = new AbortController();
-//               setTimeout(() => cancel.abort, 60_000);
-      
-//               console.log("Product and product group updates:");
-//               for await (const event of this.api.signal(cancel.signal).openEventStream(stream.id)) {
-//                 const listProduct = this.api.listProducts();
-//                 const listGroups = this.api.listProductGroups();
-//                 (await listProduct).forEach(
-//                   (element) =>
-//                     this.db.query<DBQuery[]>`update onslip.products SET (name, price, description, productcategory_id) = (${element.name}, ${element.price ?? null}, ${element.description ?? null}, ${element["product-group"]}) where rowid = ${element.id}`
-//                 );
-//                 (await listGroups).forEach(
-//                   (element) =>
-//                     this.db.query<DBQuery[]>`update onslip.products SET (name) = (${element.name}) where rowid = ${element.id}`
-//                 );
-//                 console.log(event);
-//               }
-//               console.log("All done");
-//           }
-//           catch(error)
-//           {
-//               console.error(error);
-//           }
-//       }
-//   }
-
-  private async ListenerDelete()
-  {
-      while(true)
-      {
-          try{
-            const stream = await this.api.addEventStream({
-                state: "pending",
-                queries: [
-                  { resource: "records", query: "usage.type=product-delete" },
-                  { resource: "records", query: "usage.type=productgroup-delete" },
-                ],
-              });
-              
-              const cancel = new AbortController();
-              setTimeout(() => cancel.abort, 60_000);
-      
-              for await (const event of this.api
-                .signal(cancel.signal)
-                .openEventStream(stream.id)) {
-                // const listProduct = this.api.listProducts();
-                // const listGroups = this.api.listProductGroups();
-                // (await listProduct).forEach(
-                //   (element) =>
-                //     this.db.query<DBQuery[]>`DELETE from onslip.products SET (name, price, description, productcategory_id) = (${element.name}, ${element.price ?? null}, ${element.description ?? null}, ${element["product-group"]}) where rowid = ${element.id}`
-                // );
-                // (await listGroups).forEach(
-                //   (element) =>
-                //     this.db.query<DBQuery[]>`DELETE onslip.products SET (name) = (${element.name}) where rowid = ${element.id}`
-                // );
-                event.payload
-                console.log(event);
-                
-              }
-              console.log("All done");
-          }
-          catch(error)
-          {
-              console.log("error");
-          }
-      }
   }
 }
 
