8c89d3fe7b6f4413680e5fd08553859bfeee756c
Martin Blom <martin.blom@onslip.com>
2021-12-30 22:47:32 +0100

    Small example of how HTML can be generated server-side, via TSX or template literals.

---
+++ backend/src/dhm-service.tsx
@@ -0,0 +1,70 @@
+/** @jsx     jsx4HTML.element */
+/** @jsxFrag jsx4HTML.fragment */
+
+import { DatabaseURI, URI } from '@divine/uri';
+import { html, jsx4HTML } from '@divine/uri-x4e-parser';
+import { WebArguments, WebResource, WebResponse, WebService, WebStatus } from '@divine/web-service';
+import { API } from '@onslip/onslip-360-node-api';
+import { DHMConfig } from './schema';
+
+export class DHMService {
+    private api: API;
+    private db: DatabaseURI;
+
+    constructor(private config: DHMConfig) {
+        const { base, realm, id, key } = config.onslip360;
+        this.api = new API(base, realm, id, key);
+        this.db  = new URI(config.database.uri) as DatabaseURI;
+    }
+
+    async initialize(): Promise<this> {
+        return this;
+    }
+
+    asWebService(): WebService<this> {
+        const svc = this;
+
+        return new WebService(this)
+            .addResource(class implements WebResource {
+                static path = RegExp('');
+
+                async GET(args: WebArguments) {
+                    return svc.rootResponse(args.string('?who', undefined));
+                }
+            })
+            .addResource(class implements WebResource {
+                static path = /(?<type>literal|tsx)/;
+
+                async GET(args: WebArguments) {
+                    const [ _hello, name, version ] = await svc.rootResponse(args.string('?who', undefined));
+
+                    if (args.string('$type') === 'literal') {
+                        return new WebResponse(WebStatus.OK, html`<html>
+                            <h1>Hello, ${name}</h1>
+                            <p>DB Version: ${version}</p>
+                        </html>`, {
+                            'content-type': 'text/html',
+                        });
+                    } else {
+                        return new WebResponse(WebStatus.OK, <html>
+                            <h1>Hello, { name }</h1>
+                            <p>DB Version: { version }</p>
+                        </html>, {
+                            'content-type': 'text/html',
+                        });
+                    }
+                }
+            });
+    }
+
+    private async rootResponse(who?: string) {
+        const clientInfo = await this.api.getClientInfo()
+        const dbVersion  = await this.db.query<DBVersion[]>`select version()`;
+
+        return [ 'Hello', who ?? clientInfo.user?.name, dbVersion[0].version ];
+    }
+}
+
+interface DBVersion {
+    version: string;
+}
