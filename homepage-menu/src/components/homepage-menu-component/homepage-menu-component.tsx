009ff03628656d53a061945dd550893a0ffeaf10
pajo30 <jonsenpatrik@gmail.com>
2022-01-25 08:44:17 +0100

    merge conflict fixed

--- homepage-menu/src/components/homepage-menu-component/homepage-menu-component.tsx
+++ homepage-menu/src/components/homepage-menu-component/homepage-menu-component.tsx
@@ -1,15 +1,6 @@
 import { Component, h, State, Prop } from '@stencil/core';
-<<<<<<< HEAD
 import { productsWithCategory } from '../../utils/utils';
 import '@ionic/core'
-import { ProductComponent } from '../product-component/product-component';
-=======
-<<<<<<< HEAD
-import { IonicSlides } from '@ionic/core';
-=======
-import '@ionic/core'
->>>>>>> 1ccd487e9bd1a71429d5d5ce8a55c425ddd4f570
->>>>>>> 03fef285f9ac9f11cee653b5f3e61aca343e681b
 
 @Component({
   tag: 'homepage-menu-component',
@@ -37,7 +28,6 @@ export class HomepageMenuComponent {
   render() {
     return (
       <html>
-<<<<<<< HEAD
         <ion-button></ion-button>
         <table>
           {
@@ -63,7 +53,6 @@ export class HomepageMenuComponent {
             })
           }
         </table>
-=======
         {
           this.responsedata.map(p => {
             return (
@@ -78,7 +67,6 @@ export class HomepageMenuComponent {
             )
           })
         }
->>>>>>> 1ccd487e9bd1a71429d5d5ce8a55c425ddd4f570
       </html>
     )
   }
