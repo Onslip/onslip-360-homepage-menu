6937c714bd024a9a528f0a1dccaf1b39398ac223
elisbihl <elis.bihl.syne20lin@tucsweden.se>
2022-02-04 12:01:47 +0100

    second try

--- backend/src/dhm-service.tsx
+++ backend/src/dhm-service.tsx
@@ -13,16 +13,12 @@ export class DHMService {
     private api: API;
     private db: DatabaseURI;
     private listener: Listener;
-    private response: WebResponse;
 
     constructor(private config: DHMConfig) {
         const { base, realm, id, key } = config.onslip360;
         this.api = new API(base, realm, id, key);
         this.db = new URI(config.database.uri) as DatabaseURI;
         this.listener = new Listener(this.api, this.db);
-        this.response = new WebResponse(WebStatus.OK);
-
-
     }
 
     async initialize(): Promise<this> {
@@ -41,15 +37,15 @@ export class DHMService {
 
                     return svc.rootResponse();
                 }
-
-
             })
 
             .addResource(class implements WebResource {
                 static path = /upload/;
 
                 async POST(args: WebArguments) {
-                    return [console.log(args.body())]
+                    let api: newApi
+
+                    return [console.log(args.log)]
                 }
             })
 
@@ -61,17 +57,17 @@ export class DHMService {
     }
 
 
-    private async WritetoFile(base: string, realm: string, id: string, key: string) {
+    private async WritetoFile(base: string, realm: string, id: string, key: string, dbURI: string) {
         this.api = new API(base, realm, id, key);
         writeFileSync('./test.toml', `[listen]
 host  = 'localhost'
 port  = 8080
 
 [database]
-uri   = 'postgresql://user:password@free-tier5.gcp-europe-west1.cockroachlabs.cloud:26257/defaultdb?sslmode=require&options=--cluster%3Donslip-360-dhm-2447'
+uri   = '${dbURI}'
 
 [onslip360]
-base  = 'https://test.onslip360.com/v1/'      # Onslip 360 environment
+base  = '${base}'      # Onslip 360 environment
 realm = '${realm}'                          # Onslip 360 account
 id = '${id}' # ID of user's API key
 key = '${key}'                                    # User's Base64-encoded API key
@@ -124,4 +120,12 @@ interface productsWithCategory {
         price: string,
         description: string
     }[]
+}
+
+interface newApi {
+    base: string,
+    realm: string,
+    key: string,
+    id: string,
+    uri: string
 }
