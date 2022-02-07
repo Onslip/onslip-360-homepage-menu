c42f009593aa6ed6124277fe4b4295a3c527dcab
elisbihl <elis.bihl.syne20lin@tucsweden.se>
2022-02-07 13:31:52 +0100

    background.json save images

---
+++ homepage-menu/stencil.config.ts
@@ -0,0 +1,32 @@
+import { Config } from '@stencil/core';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
+
+
+export const config: Config = {
+  namespace: 'homepage-menu',
+  globalStyle: 'src/global/global.css',
+
+  rollupPlugins: {
+
+    after: [
+      nodePolyfills(),
+    ]
+  },
+
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
