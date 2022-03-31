e6ffbd53599e352c06c9c7a2fa5a6382ea348660
pajo30 <jonsenpatrik@gmail.com>
2022-03-31 14:34:02 +0200

    crop basic

--- frontend/src/components/Editor/crop-tool/crop-tool.tsx
+++ frontend/src/components/Editor/crop-tool/crop-tool.tsx
@@ -12,22 +12,20 @@ export class CropTool {
   @Prop() MaxWidth: number;
   @State() Width: number;
   @State() Height: number;
-  private canvas;
   private img = new Image();
 
-  componentWillLoad() {
+  async componentWillLoad() {
     this.Width = this.MaxWidth
     this.Height = this.Width / this.AspectRatio;
     this.img.src = this.Image;
 
+    this.img.onload = () => {
+      this.Compress();
+    }
   }
 
-  componentDidRender() {
-
-    this.canvas = this.element.shadowRoot.querySelector('canvas');
+  async componentDidRender() {
     // this.canvas.height = (this.canvas.width / this.AspectRatio);
-
-    this.Compress();
   }
 
   async Resize() {
@@ -47,8 +45,37 @@ export class CropTool {
     // newCanvas.width = this.img.width;
     // newCanvas.height = this.img.height;
     // newCanvas.getContext('2d').drawImage(this.img, 0, 0, newCanvas.width, newCanvas.height)
-    this.canvas.getContext('2d').drawImage(this.img, 0, 0, this.img.width, this.img.height, 0, 0, this.Width, this.Height);
-    console.log(this.canvas.image);
+    // this.canvas.getContext('2d').drawImage(this.img, 0, 0, this.img.width, this.img.height, 0, 0, this.Width, this.Height);
+    // console.log(this.canvas.image);
+
+    const main = this.element.shadowRoot.querySelector('.mainElement')
+
+    let canvas = document.createElement("canvas");
+    canvas.width = this.img.width;
+    canvas.height = this.img.height;
+    let ctx = canvas.getContext("2d");
+    ctx.drawImage(this.img, 0, 0);
+    let imageData = ctx.getImageData(65, 60, this.Width, this.Height);
+
+    main.appendChild(canvas)
+
+    // create destiantion canvas
+    let canvas1 = document.createElement("canvas");
+    canvas1.width = 100;
+    canvas1.height = 100;
+    let ctx1 = canvas1.getContext("2d");
+    ctx1.rect(0, 0, this.Width, this.Height);
+    ctx1.fillStyle = 'white';
+    ctx1.fill();
+    ctx1.putImageData(imageData, 0, 0);
+
+    main.appendChild(canvas1)
+
+
+    // put data to the img element
+    let dstImg = document.createElement('img');
+    dstImg.src = canvas1.toDataURL("image/png");
+    // main.appendChild(dstImg)
   }
 
   render() {
@@ -56,7 +83,7 @@ export class CropTool {
       <Host>
         <div class='mainElement'>
           {/* <div on={() => this.Resize()} class='resizeElement' onClick={() => this.Compress('resizeElement')}> */}
-          <canvas class='mainCanvas'></canvas>
+          {/* <canvas class='mainCanvas'></canvas> */}
           {/* </div> */}
         </div>
       </Host >
