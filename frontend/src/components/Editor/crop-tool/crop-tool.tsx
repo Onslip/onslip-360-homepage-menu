d9ff2910ec05e71af08b6a14f6042c71e45025b8
pajo30 <jonsenpatrik@gmail.com>
2022-04-05 14:08:26 +0200

    Crop tool progress. Can now crop and resize from any image

--- frontend/src/components/Editor/crop-tool/crop-tool.tsx
+++ frontend/src/components/Editor/crop-tool/crop-tool.tsx
@@ -1,5 +1,4 @@
 import { Component, Host, h, Prop, Element, State } from '@stencil/core';
-import { off } from 'process';
 
 @Component({
   tag: 'crop-tool',
@@ -11,69 +10,106 @@ export class CropTool {
   @Element() element: HTMLElement;
   @Prop() AspectRatio: number;
   @Prop() MaxWidth: number;
-  @State() Width: number;
-  @State() Height: number;
-  @State() startX: number = 0;
-  @State() startY: number = 0;
-  @State() endX: number = 0;
-  @State() endY: number = 0;
-  @State() x: number = 0;
-  @State() y: number = 0;
   private img = new Image();
-  private scale: number = 5
-
+  private scale: number = 1
+  
   async componentWillLoad() {
-    this.Width = this.MaxWidth
-    this.Height = this.Width / this.AspectRatio;
-
+    // this.Width = this.MaxWidth
+    // this.Height = this.Width / this.AspectRatio;
+    
     this.img.src = this.Image;
     this.img.onload = () => {
-      this.Compress();
+      this.Compress(0, 0, 0, 0);
     }
+  
   }
-
   async componentDidRender() {
     // this.canvas.height = (this.canvas.width / this.AspectRatio);
-  }
-
-  async Resize() {
-    let height = this.element.shadowRoot.querySelector('div').style.height;
-    height = height.slice(0, height.length - 2);
-    let width = Number(height) * this.AspectRatio;
-    this.element.shadowRoot.querySelector('div').style.width = `${width}px`
+    const element = this.element.shadowRoot.getElementById('resize')
+    if (this.AspectRatio <= 1) {
+      element.style.resize = 'vertical'
+    }
+    else {
+      element.style.resize = 'horizontal'
+    }
+    element.style.width = `${this.img.width * 0.5}px`
+    element.style.height = `${this.img.width * 0.5 / this.AspectRatio}px`
+  
   }
   
-  async Move(event: MouseEvent) {
-      this.startX = event.clientX
-      this.startY = event.clientY
-      console.log('Cursor at: ' + this.x + ', ' + this.x)
-  }
+  async dragElement(elmnt: HTMLElement) {
+    // const elmnt = this.element.shadowRoot.getElementById(id).parentElement
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    // if (document.getElementById(elmnt.id + "header")) {
+    //   // if present, the header is where you move the DIV from:
+    //   document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    // } else {
+    //   // otherwise, move the DIV from anywhere inside the DIV:
+    // }
+    elmnt.parentElement.ondragstart = dragMouseDown;
 
-  async MoveEnd(event: any) {
-    this.endX = event.clientX
-    this.endY = event.clientY;
-    this.x += this.endX - this.startX 
-    this.y += this.endY - this.startY
-
-    this.x = await this.limit(this.x, this.img.width * this.scale - 10, -this.img.width * this.scale + 10)
-    this.y = await this.limit(this.y, this.img.height * this.scale - 10, -this.img.height * this.scale + 10)
-    console.log('Cursor at: ' + this.endX + ', ' + this.endY)
-    console.log('Difference: ' + this.x + ', ' + this.y)
-    this.Compress()
-  }
-
-  async limit(value:number, maxLimit: number, minLimit: number): Promise<number> {
-    if (value > maxLimit) {
-      return maxLimit
+    function dragMouseDown(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // get the mouse cursor position at startup:
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      // call a function whenever the cursor moves:
+      document.onmousemove = elementDrag;
     }
-    else if (value < minLimit) {
-      return minLimit
+
+    function elementDrag(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // calculate the new cursor position:
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      // set the element's new position:
+      elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+      elmnt.style.top = `${limit(elmnt.offsetTop, elmnt.parentElement.clientHeight - elmnt.clientHeight - 4, 0)}px`
+      elmnt.style.left = `${limit(elmnt.offsetLeft, elmnt.parentElement.clientWidth - elmnt.clientWidth - 4, 0)}px`
     }
-    return value
+
+    function closeDragElement() {
+
+
+      // stop moving when mouse button is released:
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+
+    function limit(value: number, maxLimit: number, minLimit: number):number {
+      if (value > maxLimit) {
+        return maxLimit
+      }
+      else if (value < minLimit) {
+        return minLimit
+      }
+      return value
+    }
+    this.resize()
   }
-  
-  async Compress() {
-    this.Height = this.Width / this.AspectRatio;
+
+
+  async resize() {
+    // const element = this.element.shadowRoot.getElementById('resize')
+    // const width = Number(element.style.width.slice(0, element.style.width.length - 2))
+    // element.style.height = `${width / this.AspectRatio}px`¨
+    const elmnt: HTMLElement = this.element.shadowRoot.getElementById('resize')
+    if (elmnt.clientWidth < elmnt.parentElement.clientWidth && this.AspectRatio <= 1) {
+      elmnt.style.width = `${elmnt.clientHeight * this.AspectRatio}px`
+    }
+    if (elmnt.clientHeight < elmnt.parentElement.clientHeight && this.AspectRatio > 1) {
+      elmnt.style.height = `${elmnt.clientWidth / this.AspectRatio}px`
+    }
+    this.Compress(elmnt.offsetLeft, elmnt.offsetTop, elmnt.clientWidth, elmnt.clientHeight)
+  }
+
+  async Compress(targetX: number, targetY: number, targetWidth: number, targetHeight: number) {
     // const newCanvas = document.createElement('canvas');
     // newCanvas.width = this.img.width;
     // newCanvas.height = this.img.height;
@@ -82,25 +118,23 @@ export class CropTool {
     // console.log(this.canvas.image);
     const main = this.element.shadowRoot.querySelector('.mainElement')
     const result = this.element.shadowRoot.querySelector('.result')
-    const scale = this.Width / this.img.width
     let canvas = document.createElement("canvas");
-    canvas.width = this.img.width;
-    canvas.height = this.img.height;
+    const imageAspectRation: number = this.img.width / this.img.height  
+    canvas.width = 500 * this.scale;
+    canvas.height = 500 / imageAspectRation * this.scale;
     let ctx = canvas.getContext("2d");
-    ctx.drawImage(this.img, 0, 0, canvas.width, canvas.height, this.x - canvas.width / 2, this.y - canvas.height / 2, this.img.width * this.scale, this.img.height * this.scale);
-    ctx.strokeRect(0, 0, this.img.width * scale, this.img.width/this.AspectRatio * scale)
+    ctx.drawImage(this.img, 0, 0, canvas.width / this.scale, canvas.height / this.scale, 0, 0, this.img.width * this.scale, this.img.height * this.scale);
 
     main.replaceChild(canvas, main.childNodes[0])
 
-    const vOffset = (canvas.height - (canvas.width / this.AspectRatio)) / 2
-
     let canvas1 = document.createElement("canvas");
-    canvas1.width = this.Width;
-    canvas1.height = this.Height;
+
+    canvas1.width = this.MaxWidth;
+    canvas1.height = this.MaxWidth / this.AspectRatio;
     let ctx1 = canvas1.getContext("2d");
-    ctx1.drawImage(this.img, this.x - canvas.width / 2, this.y - this.img.height / 2, canvas.width * this.scale, canvas.height * this.scale);
-    
-    
+    ctx1.drawImage(this.img, targetX / this.scale, targetY / this.scale, targetWidth / this.scale, targetHeight / this.scale, 0, 0, canvas1.width, canvas1.height);
+
+
     // put data to the img element
     let dstImg = document.createElement('img');
     dstImg.src = canvas1.toDataURL("image/png");
@@ -110,9 +144,17 @@ export class CropTool {
   render() {
     return (
       <Host>
-        <div class='mainElement' onMouseDown={(event: MouseEvent) => this.Move(event)} onMouseUp={(event: MouseEvent) => this.MoveEnd(event)}>
-          <canvas></canvas>
+        <div class="content">
+          <div class="resizeContainer">
+            <div class='mainElement' >
+              <canvas></canvas>
+            </div>
+            <div class="resize" id='resize' onMouseMove={(event: any) => this.resize()}>
+              <div class={'drag'} draggable={true} onDrag={(event: any) => this.dragElement(event.target.parentElement)}></div>
+            </div>
+          </div>
         </div>
+
         <div class="result">
           <img></img>
         </div>
