1ec898c0344ed2f4493655385ce1e3d577623676
elisbihl <elis.bihl.syne20lin@tucsweden.se>
2022-04-19 22:28:16 +0200

    Some changes to schedule tool

--- frontend/src/components/Editor/modals/schedule-overlay/schedule-overlay.tsx
+++ frontend/src/components/Editor/modals/schedule-overlay/schedule-overlay.tsx
@@ -8,16 +8,18 @@ import { MenuWithCategory } from '../../../utils/utils';
 })
 export class ScheduleOverlay {
   @State() menus: MenuWithCategory[]
-  @State() LocationList;
-  @State() StartTime
-  @State() DaysOfWeek: string[] = ['Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lördag', 'Söndag']
-  @State() Hours: string[] = ['01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00']
+  @State() locationList;
+  @State() daysOfWeek: string[] = ['Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lördag', 'Söndag']
+  @State() hours: string[] = ['01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00']
+  @State() selectedMenu: string;
+  @State() selectedLocation: string;
+
 
   async componentWillLoad() {
     const menu = document.querySelector('editor-visual-check').shadowRoot.querySelector('homepage-menu-editor-component').shadowRoot.querySelector('menu-editor-component');
     const toolbar = document.querySelector('editor-visual-check').shadowRoot.querySelector('toolbar-component');
     this.menus = await menu?.GetMenu();
-    this.LocationList = await toolbar?.GetLocations();
+    this.locationList = await toolbar?.GetLocations();
   }
 
   async close() {
@@ -27,7 +29,13 @@ export class ScheduleOverlay {
   }
 
   SelectTime(ev) {
-    ev.target.style.backgroundColor = 'Red'
+    console.log(ev.target.className)
+
+    ev.onmousemove = (ev: any) => {
+
+      ev.target.style.backgroundColor = 'Red'
+
+    }
   }
 
   private customPopoverOptions: any = {
@@ -48,45 +56,42 @@ export class ScheduleOverlay {
             </ion-row>
             <ion-row>
               <ion-item class='row'>
-                <ion-select value={''} interface='popover' interfaceOptions={this.customPopoverOptions} placeholder='Plats'>
-                  {this.LocationList.locations.map(x => <ion-select-option value={x}>{x}</ion-select-option>)}
+                <ion-select value={this.locationList.selectedLocation} interface='popover' interfaceOptions={this.customPopoverOptions} placeholder='Plats' onIonChange={(event: any) => this.selectedLocation = event.target.value}>
+                  {this.locationList.locations.map(x => <ion-select-option value={x}>{x}</ion-select-option>)}
                 </ion-select>
               </ion-item>
               <ion-item class='row'>
-                <ion-select value={''} interface='popover' interfaceOptions={this.customPopoverOptions} placeholder='Meny'>
+                <ion-select value={this.selectedMenu} interface='popover' interfaceOptions={this.customPopoverOptions} placeholder='Meny' onIonChange={(event: any) => this.selectedMenu = event.target.value}>
                   {this.menus.map(x => <ion-select-option value={x}>{x.menu.name}</ion-select-option>)}
                 </ion-select>
               </ion-item>
             </ion-row>
-
           </ion-col>
         </div>
         <div class="body">
-
           <div class="content">
             <ion-row class='HeaderRow'>
               <table class='HeaderTable'>
                 <tbody>
                   <tr class='DaysOfWeekHeaders'>
                     <th class='Days'></th>
-                    {this.DaysOfWeek.map(x => <th class='Days'>{x}</th>)}
+                    {this.daysOfWeek.map(x => <th class='Days'>{x}</th>)}
                   </tr>
                 </tbody>
               </table></ion-row>
             <div class='scroll'>
               <table class='Schedule'>
-                <tbody class='TableBody'>
+                <tbody class='TableBody' onMouseDown={(event: any) => this.SelectTime(event)}>
                   <tr class='TimeSlots'>
                     <td class='Time'><div></div></td>
-                    {this.DaysOfWeek.map(() => <td class='box' onMouseDown={(event: any) => this.SelectTime(event)}></td>)}
+                    {this.daysOfWeek.map(() => <td class='box'></td>)}
                   </tr>
                   {
-                    this.Hours.map(x => <tr class='TimeSlots'>
+                    this.hours.map(x => <tr class='TimeSlots'>
                       <td class='Time'><div class='TimeText'>{x}</div></td>
-                      {this.DaysOfWeek.map(() => <td class='box' onMouseDown={(event: any) => this.SelectTime(event)}></td>)}
+                      {this.daysOfWeek.map(() => <td class='box'></td>)}
                     </tr>)
                   }
-
                 </tbody>
               </table>
             </div>
